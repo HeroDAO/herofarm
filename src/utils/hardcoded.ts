cdde667137500b7819a48f3e7789ad76eca5f8f5
jordanlesich <jordan.lesich@gmail.com>
2021-03-08 15:10:45 -0500

    fixed farm page text

--- src/utils/hardcoded.ts
+++ src/utils/hardcoded.ts
@@ -1,39 +1,50 @@
 import hausIcon from '../assets/img/haus__icon.png'
 import hausEthIcon from '../assets/img/haus-eth__icon.png'
 
+const FARM_ID = {
+  SHOGUN: 'fHAUS-WETH UNI-V2 LP',
+  RONAN: 'fHAUS-undefined UNI-V2 LP',
+}
+
 interface PoolData {
   name: string
   p1: string
   p2: string
   icon: any
+  earnToken?: string
+  stakeToken?: string
+  url?: string
+  subtitle?: string
 }
 
 const shogun = {
   name: 'Shogun',
   p1: 'Stake $HAUS/ETH Uni LP',
   p2: 'Get 2x Rewards, Soft Signaling',
+  subtitle: 'Stake HAUS/ETH UNI V2 LP to earn more HAUS',
   icon: hausEthIcon,
+  earnToken: 'HAUS',
+  stakeToken: 'ETH/HAUS',
+  url: 'google.com',
 }
 const ronan = {
   name: 'Ronan',
   p1: 'Stake $HAUS on your own',
   p2: 'Get 1x rewards, Soft Signaling',
+  subtitle: 'Stake HAUS to earn more HAUS',
   icon: hausIcon,
+  earnToken: 'HAUS',
+  stakeToken: 'HAUS',
+  url: 'google.com',
 }
 
 export const getHardCodedData = (farm: any): PoolData => {
-  if (farm.id === 'fHAUS-WETH UNI-V2 LP') {
-    return {
-      ...farm,
-      ...shogun,
-    }
-  } else if (farm.id === 'fHAUS-undefined UNI-V2 LP') {
-    return {
-      ...farm,
-      ...ronan,
-    }
+  if (farm?.id === FARM_ID.SHOGUN || farm === FARM_ID.SHOGUN) {
+    return shogun
+  } else if (farm?.id === FARM_ID.RONAN || farm === FARM_ID.RONAN) {
+    return ronan
   } else {
-    return { ...farm, description: 'No description' }
+    return { ...farm, description: 'No description', icon: null }
   }
 }
 
