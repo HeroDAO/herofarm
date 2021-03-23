f7249d9f6721a3c92c0f664aa5a5acf410b28d98
skuhlmann <samkuhlmann@gmail.com>
2021-03-23 14:58:38 -0600

    trash test

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@
 // export const HONEYSWAP_GQL_URI =
 //   'https://api.thegraph.com/subgraphs/name/skuhlmann/uniswap-v2-kovan'
 export const HONEYSWAP_GQL_URI =
-  'https://api.thegraph.com/subgraphs/name/1hive/honeycomb'
+  'https://api.thegraph.com/subgraphs/name/1hive/uniswap-v2'
 export const HONEYCOMB_GQL_URI =
   'https://api.thegraph.com/subgraphs/name/odyssy-automaton/daohaus-honey-haus-xdai'
 // 'https://api.thegraph.com/subgraphs/name/odyssy-automaton/daohaus-honey-haus-kovan'
